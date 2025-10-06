fdf3b14902ad6e7a0cd92b5f5d4f5b823ca714ad
GavinLakner <gavinlakner@icloud.com>
2025-10-06 03:39:24 +0000

    Done ish

--- WordCount.py
+++ WordCount.py
@@ -12,17 +12,17 @@ def main():
   for line in textFile:
     lineCount = lineCount + 1
     words = line.split()
-    letters = words.split()
+    letters = line.split() #don't know what to split it by
     for w in words:
-      wordCount = wordCount + 1
-      for l in letters:
-        letterCount = letterCount + 1
-  
+      wordCount = wordCount + 1 
+    for l in letters:
+      letterCount = letterCount + 1
 
   print("Lines:" , lineCount)
   print("Words:" , wordCount)
   print("Letters:" , letterCount)
-
+#nothing in notes or lecture over how to find individual letter count and 
+#google wants me to add a 10 lines of code for it so idk
 
 if __name__ == '__main__':
   main()
